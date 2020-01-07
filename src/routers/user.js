bbb21d976150c14dd4b9a78d2bf91849a832caaa
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-01-06 19:21:07 -0500

    finished section 14

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,6 +2,21 @@ const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
+const multer = require('multer')
+const sharp = require('sharp')
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload image'))
+    }
+
+    cb(undefined, true)
+  }
+})
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
@@ -51,6 +66,15 @@ router.post('/users/logoutAll', auth, async (req, res) => {
   }
 })
 
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+  const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+  req.user.avatar = buffer
+  await req.user.save()
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
+})
+
 router.get('/users/me', auth, async (req, res) => {
   res.send(req.user)
 })
@@ -80,6 +104,27 @@ router.delete('/users/me', auth, async (req, res) => {
     res.send(req.user)
   } catch (e) {
     res.status(500).send()
+  }
+})
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  await req.user.save()
+  res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user || !user.avatar) {
+      throw new Error()
+    }
+
+    res.set('Content-type', 'image/png')
+    res.send(user.avatar)
+  }
+  catch (e) {
+    res.status(404).send()
   }
 })
 
